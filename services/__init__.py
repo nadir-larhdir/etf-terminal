f0d3d67059141aa1fa35138ffa30bccb65958360
Nadir <nadir.larhdir@gmail.com>
2026-04-02 19:30:42 -0400

    Add FRED macro data client and service layer

--- services/__init__.py
+++ services/__init__.py
@@ -1,9 +1,12 @@
 from services.admin import TickerManagerService
 from services.analytics import AnalyticsService
+from services.macro import FredClient, MacroDataService
 from services.market import MarketDataService
 
 __all__ = [
     "AnalyticsService",
+    "FredClient",
     "MarketDataService",
+    "MacroDataService",
     "TickerManagerService",
 ]
